249a97780f3c90d7bbc203d9332690bd7bc12e96
docsa <alain.couchot@gmail.com>
2016-04-24 19:50:26 +0200

    Ajout alerte

--- js/main.js
+++ js/main.js
@@ -88,3 +88,21 @@ function conflict () {
   }
   return false
 }
+function DlgShow (Message) {
+  // Change the message.
+  // var Msg = document.getElementById("DlgContent")
+  // Msg.innerHTML = Message
+  // Display the dialog box.
+  var Dlg = document.getElementById("Overlay")
+  Dlg.style.visibility = "visible"
+}
+
+function DlgHide (Result) {
+  // Display the result onscreen.
+  var Output = document.getElementById("status")
+  Output.innerHTML = "You clicked: " + Result
+
+  // Hide the dialog box.
+  var Dlg = document.getElementById("Overlay")
+  Dlg.style.visibility = "hidden"
+}
